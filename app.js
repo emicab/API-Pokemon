a531fc58699475b23fb5265e5f6bfe9451cd77ab
emiliano <emicabanillas.cab@gmail.com>
2022-08-08 18:21:56 -0300

    visual

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ let btnSig = document.getElementById("btnSiguiente");
 let container = document.getElementById("container");
 let contenedor = document.getElementById("contenedor");
 
+let btnMostrar = document.querySelector('.btnMostrar');
+
+
 /* Botones Reemplazados por evento Scroll
 btnSig.addEventListener('click', () =>{
     console.log('boton sig');
@@ -17,49 +20,21 @@ btnAnt.addEventListener('click', () =>{
     offset-=10;
     obtenerPokemones()
 }) */
-
-
-
-
-let dataVisor = 0;
+let id_pokemon = 0;
 async function obtenerPokemones() {
     try {
-        const listaPokemones = await fetch(
-            `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=15`
-            );
-            const listaPokemonesData = await listaPokemones.json();
-            
-            // console.log(listaPokemonesData);
-            
-            //SI QUIERES CONSULTAR SOLO UN POKEMON
-            
-        const infoPokemonUno = await fetch(listaPokemonesData.results[dataVisor].url);
-        const infoPokemonUnoData = await infoPokemonUno.json();
-        console.log(infoPokemonUnoData)
-        pokemon = {
-            id: infoPokemonUnoData.id,
-            name: infoPokemonUnoData.name,
-            stats: {
-                hp: infoPokemonUnoData.stats[0].base_stat,
-                atk: infoPokemonUnoData.stats[1].base_stat,
-                def: infoPokemonUnoData.stats[2].base_stat,
-                atk_esp: infoPokemonUnoData.stats[3].base_stat,
-                def_esp: infoPokemonUnoData.stats[4].base_stat,
-                spd: infoPokemonUnoData.stats[5].base_stat,
-            },
-            sprites: {
-                front_default: infoPokemonUnoData.sprites.front_default,
-                back_default: infoPokemonUnoData.sprites.back_default,
-                front_shiny: infoPokemonUnoData.sprites.front_shiny,
-                back_shiny: infoPokemonUnoData.sprites.back_shiny
-            },
-            ability: {
-                ability_1: infoPokemonUnoData.abilities[0].ability.name,
-                ability_2: infoPokemonUnoData.abilities[1].ability.name
-            }
+        const listaPokemones = await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=15`);
+        const listaPokemonesData = await listaPokemones.json();
+        // console.log(listaPokemonesData);
+            let number = listaPokemonesData.results.length;
 
-        }
-        console.log(pokemon);
+        //SI QUIERES CONSULTAR SOLO UN POKEMON
+        
+        const infoPokemonUno = await fetch(listaPokemonesData.results[id_pokemon].url);
+        const infoPokemonUnoData = await infoPokemonUno.json();
+        // console.log(infoPokemonUnoData)
+        setVisorPokemon(infoPokemonUnoData)
+        
 
         //SI QUIERES CONSULTAR TODOS LOS POKEMON (PRO)
         const listaRequests = [];
@@ -69,68 +44,102 @@ async function obtenerPokemones() {
         const dataPokemon = await Promise.all(listaRequests);
 
         // console.log(dataPokemon)
-
         pintarPokemon(dataPokemon);
+        getIdPokemon()
+        
+        scroller()
         
     } catch (ex) {
         console.error(ex);
     }
 }
-container.addEventListener("scroll", (e) => {
-    let alturaScroll = container.scrollHeight - 499;
-    let scrolled = container.scrollTop;
-    
-    // console.log(`scrolled ${scrolled}`);
-    // console.log(`alturaScroll ${alturaScroll}`);
-    
-    
-    
-    if (alturaScroll === scrolled) {
-        offset += 15;
-        
-        obtenerPokemones(dataVisor);
-    }
-});
+
+const scroller = () =>{
+    container.addEventListener("scroll", (e) => {
+        let alturaScroll = container.scrollHeight - 499;
+        let scrolled = container.scrollTop;   
+        // console.log(`scrolled ${scrolled}`);
+        // console.log(`alturaScroll ${alturaScroll}`);
+        if (alturaScroll === scrolled) {
+            offset += 15;
+            obtenerPokemones();
+        }
+    });
+}
+
 function pintarPokemon(dataPokemon) {
+    console.log(dataPokemon);
+    let visorPokemon = document.getElementById('visorPokemon');
+    visorPokemon.innerHTML = `<img src="${dataPokemon[0].sprites.front_default}" alt="">`
+
     dataPokemon.forEach((poke) => {
         container.innerHTML += `
     <div class="card-contenedor">
         <div class="cardPokemon">
             <div class="cardPokemon--Datos">
-                <span class="id_pokemon">ID#${poke.id}</span>
+                <span id="pokemonID_${poke.id}" class="id_pokemon">ID#${poke.id}</span>
                 <h3 class="cardPokemon--Datos_nombre">${poke.name}</h3>
                 <img src="${poke.sprites.front_default}" class="img_pokemon">
-                
             </div>
+            <button class="btnMostrar" id="btnMostrar"><i class="fa fa-arrow-left" aria-hidden="true"></i> Mostrar</button>
         </div>
-    </div>
-    `;
+    </div>`;
     });
 }
 
 function getImgTipos(types) {
-    const typeImg = types.map(
-        (type) =>
-            `<img src="img/tipo_${type.type.name}.jpg" alt="" class="img-fluid" width="65">`
-    );
+    const typeImg = types.map((type) =>`<img src="img/tipo_${type.type.name}.jpg" alt="" class="img-fluid" width="65">`);
     return typeImg.join("");
 }
 
-obtenerPokemones();
 
-container.addEventListener('click', (e) =>{
-    const visorPokemon = document.getElementById('visorPokemon');
-    let pokemonVisor = e.target.childNodes[1].firstElementChild.parentElement.firstElementChild.children[0].innerText;
-    
-    dataVisor = parseInt(pokemonVisor.slice(3, 10) - 1)
-    console.log(pokemonVisor);
-    
-    visorPokemon.innerHTML = `${pokemon.name}`
-    
-    obtenerPokemones(dataVisor);
-    
-})
 
+
+
+function setVisorPokemon(infoPokemonUnoData){
+    
+    // console.log(infoPokemonUnoData);
+    setpokemon = {
+        id: infoPokemonUnoData.id,
+        nombre: infoPokemonUnoData.name,
+        img: {
+            frontDefault: infoPokemonUnoData.sprites.front_default,
+            frontDhiny: infoPokemonUnoData.sprites.front_shiny,
+            backDefault: infoPokemonUnoData.sprites.back_default,
+            backDhiny: infoPokemonUnoData.sprites.back_shiny
+        },
+        altura: infoPokemonUnoData.height,
+        peso: infoPokemonUnoData.weight,
+        estadisticas: {
+            hp: infoPokemonUnoData.stats[0].base_stat,
+            atk: infoPokemonUnoData.stats[1].base_stat,
+            def: infoPokemonUnoData.stats[2].base_stat,
+            atk_sp: infoPokemonUnoData.stats[3].base_stat,
+            def_sp: infoPokemonUnoData.stats[4].base_stat,
+            spd: infoPokemonUnoData.stats[5].base_stat,
+        }
+    }
+    console.log(setpokemon);
+}
+function setPokemon(pokemonObj){
+    pokemonID = pokemonObj.querySelector('.id_pokemon').textContent
+    pokemonID = parseInt(pokemonID.slice(3, 5))
+    console.log(pokemonID);
+}
+
+function getIdPokemon(){
+    container.addEventListener('click', (e)=>{
+        if(e.target.classList.contains('btnMostrar')){
+            setPokemon(e.target.parentElement)
+            visorPokemon.innerHTML = ``
+        }
+        
+    })
+}
+function main(){
+    obtenerPokemones();
+}
+main()
 
 /* 
 <!-- Stats Pokemon -->
